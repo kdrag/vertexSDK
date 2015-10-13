495d672b31190a35c216982e81dffe9f6860d35e
John Lin <jlin@wirelessglue.com>
2015-10-13 11:54:24 -0700

    basic authentication input added

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -50,6 +50,7 @@ angular.module('vertexSDK.controllers', ['vertexSDK.services'])
 .controller("signOffController", ['$state', '$ionicHistory','$localstorage',function($state, $ionicHistory, $localstorage) {
 //not working yet
   $localstorage.set('loggedInState', false);
+  $localstorage.set('basic_auth_header', 'Basic ' );
 
 }])
 
@@ -63,14 +64,19 @@ angular.module('vertexSDK.controllers', ['vertexSDK.services'])
 
 .controller("signInController",['$rootScope', '$scope', '$http', '$state', '$localstorage', '$ionicHistory', function($rootScope, $scope, $http, $state, $localstorage, $ionicHistory) {
 
+  var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=0,c1=0,c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
+
+
   $scope.data=[];
   $scope.data = {
     "email" : null,
     "userName" : null,
-    "password" : null,
-    "confirmPassword" : null,
+    "passCode" : null,
+    "confirmCode" : null,
     "submissions" : 0,
-    "stateValue" : null
+    "stateValue" : null,
+    "userName64" : null,
+    "passCode64" : null
   };
 
   $scope.signInSubmit = function(data) {
@@ -78,13 +84,24 @@ angular.module('vertexSDK.controllers', ['vertexSDK.services'])
 
     $localstorage.set('email', $scope.data.email);
     $localstorage.set('userName', $scope.data.userName);
-    $localstorage.set('password', $scope.data.password);
-    $localstorage.set('loggedInState', true);
-    $scope.data.stateValue=$localstorage.get('loggedInState');
+    $localstorage.set('passCode', $scope.data.passCode);
+    $localstorage.set('confirmCode', $scope.data.confirmCode);
 
+    if ($localstorage.get('passCode')!=$localstorage.get('confirmCode')){
+      alert('does not match');
+      $localstorage.set('loggedInState', false);
+      $scope.data.stateValue=$localstorage.get('loggedInState');
+    }else{
+      //construct basic authentication header value; change local client log state to true
+      var header_value=$scope.data.userName+":"+$scope.data.passCode;
+      var header64=Base64.encode(header_value);
+      $localstorage.set('basic_auth_header', 'Basic '+ header64 );
+      //alert($localstorage.get('basic_auth_header'));
+      $localstorage.set('loggedInState', true);
+      $scope.data.stateValue=$localstorage.get('loggedInState');
+    }
 
-  }
-
+};
 
 }])
 
@@ -92,6 +109,10 @@ angular.module('vertexSDK.controllers', ['vertexSDK.services'])
 
 
 .controller('userInfoController', function ($scope, Event, $ionicLoading){
+
+    $scope.reloadRoute = function() {
+     $route.reload();
+    }
 
     var unix_timestamp = 1318305600000;
 
--- www/js/services.js
+++ www/js/services.js
@@ -1,51 +1,8 @@
 angular.module('vertexSDK.services',['angular-storage', 'ngResource'])
-//.constant('ENDPOINT_URI', 'http://vtx00.wgn.jp/')
-//.constant('ENDPOINT_URI', 'http://localhost:9000/')
+
 .constant('ENDPOINT_URI', 'http://localhost:8100/event')
 
-.constant('TESTAUTH','YWRtaW46YWRtaW4=')
 
-/*
-.factory('UserService', function(store) {
-	// use these services to set the client to the signed-In user
-    var service = this,
-        currentUser = null;
-
-    service.setCurrentUser = function(user) {
-        currentUser = user;
-        store.set('user', user);
-        return currentUser;
-    };
-
-    service.getCurrentUser = function() {
-        if (!currentUser) {
-            currentUser = store.get('user');
-        }
-        return currentUser;
-    };
-})
-
-.factory('APIInterceptor', function($rootScope, UserService) {
-    var service = this;
-
-    service.request = function(config) {
-        var currentUser = UserService.getCurrentUser(),
-            access_token = currentUser ? currentUser.access_token : null;
-
-        if (access_token) {
-            config.headers.authorization = access_token;
-        }
-        return config;
-    };
-
-		service.responseError = function(response) {
-        if (response.status === 401) {
-            $rootScope.$broadcast('unauthorized');
-        }
-        return response;
-		};
-})
-*/
 .factory('GetUserService', function($http, ENDPOINT_URI, TESTAUTH) {
        console.log('at GetUserService');
        var service = this,
@@ -58,51 +15,26 @@ angular.module('vertexSDK.services',['angular-storage', 'ngResource'])
        service.constantValue="check";
 
 			 service.getUser = function() {
-					 //return $http.get(getUrl()); // no parameters
           alert('at getUser');
           $http.defaults.headers.common.Authorization = "Basic " + TESTAUTH;
 					return $http.get(getUrl());
 		};
 })
+.factory('$Encode64', function(){
+ var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=0,c1=0,c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
+
+})
+
 
 .factory('Event', function ($resource, $http, $localstorage){
-  //return $resource('http://jsonplaceholder.typicode.com/posts/1');
-  //alert('getting localstorage ' + $localstorage.get('loggedInState'));
 
-  $http.defaults.headers.common.Authorization = 'Basic ' + 'VGVzdE9wZXJhdG9yOnRlc3RvcGVyYXRvcg==';
+  alert($localstorage.get('basic_auth_header'));
+  $http.defaults.headers.common.Authorization = $localstorage.get('basic_auth_header');
   return $resource('http://localhost:8100/event/Test00');
-  //return $resource('http://vtx00.wgn.jp/event/Test00');
 
 
 })
 
-/*
-.service('LoginService', function($http, ENDPOINT_URI) {
-			 var service = this,
-					 path = 'user';
-
-			 function getUrl() {
-					 return ENDPOINT_URI + path;
-			 }
-
-			 function getLogUrl(action) {
-					 return getUrl() + action;
-			 }
-
-			 service.login = function(credentials) {
-					 return $http.post(getLogUrl('login'), credentials);
-			 };
-
-			 service.logout = function() {
-					 return $http.post(getLogUrl('logout'));
-			 };
-
-			 service.register = function(user) {
-					 return $http.post(getUrl(), user);
-			 };
-	 })
-
-	 */
 
    .factory('$localstorage', ['$window', function($window) {
      return {
